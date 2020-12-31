b1b5e172b6892c06751321790dfc4b0080e55327
z13202013 <54021602+z13202013@users.noreply.github.com>
2021-01-01 02:01:58 +0800

    Update GenshinHttpRequestMessage.cs

--- GenshinDailyHelper/Client/GenshinHttpRequestMessage.cs
+++ GenshinDailyHelper/Client/GenshinHttpRequestMessage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using GenshinDailyHelper.Constant;
 
@@ -22,7 +22,11 @@ namespace GenshinDailyHelper.Client
                 throw new ArgumentException("必须设置cookie后才能请求");
             }
 
-            Headers.Add("Accept-Encoding",Config.AcceptEncoding);
+            if(!uri.AbsoluteUri.Contains(Config.GetSignAwards))
+            {
+                Headers.Add("Accept-Encoding",Config.AcceptEncoding);
+            }
+
             Headers.Add("User-Agent",Config.Ua);
             Headers.Add("Referer",Config.RefererUrl);
             Headers.Add("Cookie",cookie);
