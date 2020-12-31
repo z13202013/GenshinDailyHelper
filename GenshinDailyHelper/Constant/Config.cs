2657f27c64ff0681f06ad112f713637c5df6fb15
z13202013 <54021602+z13202013@users.noreply.github.com>
2021-01-01 02:02:46 +0800

    修改UA、添加pushplus

--- GenshinDailyHelper/Constant/Config.cs
+++ GenshinDailyHelper/Constant/Config.cs
@@ -1,4 +1,4 @@
-﻿using GenshinDailyHelper.Util;
+using GenshinDailyHelper.Util;
 
 namespace GenshinDailyHelper.Constant
 {
@@ -8,11 +8,11 @@ namespace GenshinDailyHelper.Constant
     public static class Config
     {
         public static string Ua =>
-            $"Mozilla/5.0 (Linux; Android 5.1.1; f103 Build/LYZ28N; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Safari/537.36 miHoYoBBS/{AppVersion}";
+            $"Mozilla/5.0 (Linux; Android 8.0.0; SM-C7010 Build/R16NW; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.66 Mobile Safari/537.36 miHoYoBBS/{AppVersion}";
 
         public static string AcceptEncoding => "gzip, deflate";
 
-        public static string AppVersion => "2.3.0";
+        public static string AppVersion => "2.2.1";
 
         public static string ClientType => "5";
 
@@ -23,7 +23,7 @@ namespace GenshinDailyHelper.Constant
         public static string GetDs()
         {
             var time = SafeUtil.GetCurrentTimestamp();
-            var stringRom = SafeUtil.GetRandString(6).ToLower();
+            var stringRom = SafeUtil.GetRandString(6);
             var stringAdd = $"salt={Salt}&t={time}&r={stringRom}";
             var stringMd5 = SafeUtil.UserMd5(stringAdd);
             return $"{time},{stringRom},{stringMd5}";
@@ -32,7 +32,7 @@ namespace GenshinDailyHelper.Constant
         /// <summary>
         /// 盐(AppVersion的md5得到)
         /// </summary>
-        public static string Salt => "h8w582wxwgqvahcdkpvdhbh2w9casgfl";
+        public static string Salt => SafeUtil.UserMd5(AppVersion);
 
         #region Referer
 
@@ -64,7 +64,27 @@ namespace GenshinDailyHelper.Constant
         /// 开始签到
         /// </summary>
         public static string PostSignInfo => "event/bbs_sign_reward/sign";
-
+        
+        /// <summary>
+        /// 获取签到奖励
+        /// </summary>
+        public static string GetSignAwards => "event/bbs_sign_reward/home?";
+        
+                
+        /// <summary>
+        /// Push+推送API
+        /// </summary>
+        public static string GetPushPlusApi => "http://pushplus.hxtrip.com/send";
+        
+        /// <summary>
+        /// Push+标题
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPushPlusTitle(int month)
+        {
+            return $"米游社{month}月签到";
+        } 
+        
         #endregion
     }
 }
